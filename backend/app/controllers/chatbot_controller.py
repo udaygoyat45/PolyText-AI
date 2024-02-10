bdc85065addbb579af7e5f00e06036f09b19e6de
Uday Goyat <udaygoyat@gmail.com>
2024-02-10 14:04:43 -0500

    added chatbot routes

--- backend/app/controllers/chatbot_controller.py
+++ backend/app/controllers/chatbot_controller.py
@@ -11,4 +11,20 @@ def ping_test():
 
 @chatbot.route("list")
 def list_threads():
-    j
+    threads = list(db.openai_threads.find())
+    threads_id = [thread['id'] for thread in threads]
+    return {'threads': threads_id}
+    
+
+@chatbot.route("create_thread")
+def create_thread():
+    thread = client.beta.threads.create()
+    meta_thread = {
+        'id': thread.id,
+        'created_at': thread.created_at,
+    }
+
+    print("DEBUG")
+    print(thread)
+    db.openai_threads.insert_one(meta_thread)
+    return {'thread': thread.id}
